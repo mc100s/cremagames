0d7de7bd4559c5bc46718074a3c278cb96f2e5c5
mc100s <mc100s.b@gmail.com>
2017-01-18 22:54:06 +0100

    Add of main.css and some new features in app.js

--- app.js
+++ app.js
@@ -2,15 +2,31 @@ var database = firebase.database();
 
   console.log("Hello world!");
 
-firebase.database().ref('users').on('value', function(snapshot) {
-  var users = snapshot.val();
+firebase.database().ref('/').on('value', function(snapshot) {
+  var val = snapshot.val();
+  var users = val.users;
+  var challenges = val.challenges;
 
   var div = $('#challenge');
   if (users.length > 1) {
     // div.text(users[Math.round(Math.random()*users.length)].name);
-    div.text(users[0].name);
+    // div.text(users[random(0, users.length)].name);
+    div.text(challenges[random(0, challenges.length)].text);
   }
   console.log(div);
 });
 
 
+firebase.database().ref('/').on('value', function(snapshot) {
+  var x = snapshot.val();
+  console.log(x);
+});
+
+
+
+/**
+ * Return a random number in "[a;b[" ("b" not included)
+ */
+function random(a, b) {
+  return Math.floor(Math.random()*(b-a))+a;
+}
