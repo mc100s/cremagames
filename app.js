cf6d3b31f242c00a5ed6e01115b146068b90142d
mc100s <mc100s.b@gmail.com>
2017-01-19 00:28:53 +0100

    Improvements of CSS

--- app.js
+++ app.js
@@ -1,28 +1,27 @@
 var database = firebase.database();
 
-  console.log("Hello world!");
-
 firebase.database().ref('/').on('value', function(snapshot) {
   var val = snapshot.val();
   var users = val.users;
   var challenges = val.challenges;
 
   var div = $('#challenge');
-  if (users.length > 1) {
-    // div.text(users[Math.round(Math.random()*users.length)].name);
-    // div.text(users[random(0, users.length)].name);
-    div.text(challenges[random(0, challenges.length)].text);
+
+  for (var i = 0; i < 100; i++) {
+    setTimeout(function(){
+      if (users.length > 1) {
+        // div.text(users[random(0, users.length)].name);
+        div.text(challenges[random(0, challenges.length)].text);
+      }
+    }, 10000*i);
   }
-  console.log(div);
+
 });
 
 
-firebase.database().ref('/').on('value', function(snapshot) {
-  var x = snapshot.val();
-  console.log(x);
-});
-
-
+/*****************************************************************************/
+/********************************* FUNCTIONS *********************************/
+/*****************************************************************************/
 
 /**
  * Return a random number in "[a;b[" ("b" not included)
